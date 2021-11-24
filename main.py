07598ae2c0e4be951cb71114e11b18e6b784df77
Emeli Forero <emeli.yuliana18@gmail.com>
2021-11-24 01:23:48 -0500

    Se agrega primera parte del trabajo final, la cual consiste en consumir la API con el metodo post, para subir 10 usuarios los cuales tienen una edad mayor de 18 años, luego de esto, los usuarios se listan en una tabla

---
+++ main.py
@@ -0,0 +1,6 @@
+from ventanas import Usuarios
+
+if __name__ == '__main__':
+    # When this module is run (not imported) then create the app, the
+    # frame, show it, and start the event loop.
+    usuarios = Usuarios(titulo='Listado de usuarios')
